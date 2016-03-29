ff87234787612ec2afcfae7d23588818dec3b1d3
Chen Guo <chen.guo@nomissolutions.com>
2016-03-28 23:48:45 -0700

    add settings for spark-jobserver under bin folder and the .bashrc

---
+++ spark/spark-jobserver/.bashrc
@@ -0,0 +1,7 @@
+	export SPARK_HOME=/usr/local/spark/spark-1.6.1
+	export PATH="$PATH:$SPARK_HOME/bin"
+	export SPARK_CONF_DIR=$SPARK_HOME/conf
+	export SPARK_JOBSERVER_HOME=/usr/local/spark/spark-jobserver
+#	export SPARK_MASTER_IP=127.0.0.1
+#	export SPARK_LOCAL_IP=127.0.0.1
+
---
+++ spark/spark-jobserver/bin/settings.sh
@@ -0,0 +1,14 @@
+APP_USER=hadoop
+APP_GROUP=hadoop
+INSTALL_DIR=/usr/local/spark/spark-jobserver
+LOG_DIR=/tmp/var/log/spark-jobserver
+PIDFILE=spark-jobserver.pid
+JOBSERVER_MEMORY=1G
+SPARK_VERSION=1.6.1
+MAX_DIRECT_MEMORY=512M
+SPARK_HOME=/usr/local/spark/spark-1.6.1
+SPARK_CONF_DIR=/usr/local/spark/spark-1.6.1/conf
+HADOOP_CONF_DIR=/etc/hadoop/conf
+YARN_CONF_DIR=/etc/hadoop/conf
+SCALA_VERSION=2.10.5
+
