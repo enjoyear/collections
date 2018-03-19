f4df27e9ec63c9008e842c65021d606ed1b0f9a9
Chen Guo <almightygc@gmail.com>
2018-03-19 12:41:10 -0700

    update

--- hive/udfs/Sanitize.java
+++ hive/udfs/Sanitize.java
@@ -6,8 +6,8 @@ public class Sanitize extends UDF {
   }
 
   public static String cleanse(String in) {
-    String regexSpace = "[\r\n\t\u000f]";
-    String regexDelete = "[\ufffd\u001a\u0000\\p{Cs}\\p{Cn}\u2028]";
+    String regexSpace = "[\r\n\t\u000f\u2028]";
+    String regexDelete = "[\ufffd\u001a\u0000\\p{Cs}\\p{Cn}]";
 
     if (in == null)
       return in;
